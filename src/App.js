5e31eac5f4a87cd17001e8755c954f6616d2c13c
Dileswar <dileswarkhilar0@gmail.com>
2022-09-06 00:36:11 +0530

    navbar comtemt added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 const App = () => {
   return (
     <main>
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/" element={<Login/>} ></Route>
+        <Route exact path="/home" element={<Home />}></Route>
       </Routes>
     </main>
   );
